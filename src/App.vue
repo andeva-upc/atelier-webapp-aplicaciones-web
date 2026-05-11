66ea9a2e77aecce2669c0f23e24993e02836ef82
shouydev <shouyestefanero@gmail.com>
2026-05-10 23:44:35 -0500

    feat(shared): add sidebar and toolbar templates with others configurations.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup>
 
+import Layout from "@/shared/presentation/layout/layout.vue";
+
 </script>
 
 <template>
-
+<Layout />
 </template>
 
 <style scoped>
-
 </style>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
