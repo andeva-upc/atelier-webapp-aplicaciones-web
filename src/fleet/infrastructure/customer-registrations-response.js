ef29c80e102bbdc6efc4f1ed2b529d87cc98b990
Jennifer <u20241c998@upc.edu.pe>
2026-06-18 21:52:32 -0500

    feat(fleet): implement customer registrations API and assembler

---
+++ src/fleet/infrastructure/customer-registrations-response.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {object} CustomerRegistrationResponse
+ * @property {string} id
+ * @property {string} branch_id
+ * @property {string} customer_id
+ * @property {string} registered_at
+ * @property {string | null} [unregistered_at]
+ */
+
+/**
+ * @typedef {object} CustomerRegistrationsListResponse
+ * @property {CustomerRegistrationResponse[]} data
+ */
